aba3b39760a70a0f1754a90774d6d2fbd3856062
johnsen <johnsen@cryptape.com>
2018-07-06 15:44:25 +0800

    add java code for sqlite

---
+++ db/sqlite/Sql.java
@@ -0,0 +1,94 @@
+import java.sql.*;
+
+public class Sql {
+
+    public static void main( String args[] )
+    {
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+
+            System.out.println("--------------create table--------------");
+            Statement stmt = null;
+            stmt = c.createStatement();
+            String sql = "drop table if exists COMPANY; CREATE TABLE COMPANY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+
+            System.out.println("--------------insert data--------------");
+            stmt = c.createStatement();
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                    "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                    "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                    "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                    "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
+            stmt.close();
+
+            System.out.println("--------------select data--------------");
+            c.setAutoCommit(false);
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
+
+            rs.close();
+            stmt.close();
+
+            System.out.println("--------------update data--------------");
+            stmt = c.createStatement();
+            sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1;";
+            stmt.executeUpdate(sql);
+            c.commit();
+
+            rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("id");
+                String  name = rs.getString("name");
+                int age  = rs.getInt("age");
+                String  address = rs.getString("address");
+                float salary = rs.getFloat("salary");
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "AGE = " + age );
+                System.out.println( "ADDRESS = " + address );
+                System.out.println( "SALARY = " + salary );
+                System.out.println();
+            }
+            rs.close();
+            stmt.close();
+
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
